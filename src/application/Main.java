e2f4ba5f9e69e9333516620dee57ccf87d7f109d
josuegutilla <gutillaj@gmail.com>
2024-05-15 20:00:46 -0300

    Classe seller criada

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "Celulares");
+        Seller seller = new Seller(2, "Josué", "gutillaj@gmail.com", new Date(), 3000, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
