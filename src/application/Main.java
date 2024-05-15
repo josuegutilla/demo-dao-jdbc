61bcec7a2cc32a3f353311a9f757eab4d9ec56ed
josuegutilla <gutillaj@gmail.com>
2024-05-15 19:43:33 -0300

    Classe departamento criada

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "Celulares");
+        System.out.println(obj);
+    }
+}
---
+++ src/model/entities/Department.java
@@ -0,0 +1,50 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private int id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department that)) return false;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "id= " + id + ", name= " + name;
+    }
+}
