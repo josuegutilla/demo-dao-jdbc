9a3f617b2cccee7e71d9ad502eb9a534303940dd
josuegutilla <gutillaj@gmail.com>
2024-05-15 20:13:40 -0300

    Interfaces DepartmentDao e SellerDao

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    public void insert(Department obj);
+    public void update(Department obj);
+    public void deleteById(int id);
+    public Department findById(int id);
+    public List<Department> findAll();
+}
---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    public void insert(Seller obj);
+    public void update(Seller obj);
+    public void deleteById(int id);
+    public Seller findById(int id);
+    public List<Seller> findAll();
+}
